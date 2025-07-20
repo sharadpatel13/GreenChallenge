e034a1b5884b19d4f94092571a3734e988348355
sharadpatel13 <patelsharad602@gmail.com>
2025-07-20 14:52:50 -0400

    Fix SubmitProofForm field names and update submit_proof.html template

--- badges/forms.py
+++ badges/forms.py
@@ -4,8 +4,8 @@ from .models import UserProofUpload
 class SubmitProofForm(forms.ModelForm):
     class Meta:
         model = UserProofUpload
-        fields = ['challenge', 'proof']
+        fields = ['challenge_title', 'file_upload']
         widgets = {
-            'challenge': forms.Select(attrs={'class': 'form-control'}),
-            'proof': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'challenge_title': forms.TextInput(attrs={'class': 'form-control'}),
+            'file_upload': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
